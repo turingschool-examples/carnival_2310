1eb78b7164b4359d9f2ae723d3c961dc8eb64ad1
Stephen Nash <stephen.aa.nash@gmail.com>
2023-11-14 12:16:56 -0500

    test: Create test for total_revenue method

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Ride do
 
   before(:each) do
     @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+    @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
   end
 
   it 'exists' do
@@ -19,15 +21,22 @@ RSpec.describe Ride do
   end
 
   it 'can board riders into the rider log' do
-    @visitor1 = Visitor.new('Bruce', 54, '$10')
-    @visitor2 = Visitor.new('Tucker', 36, '$5')
     @visitor1.add_preference(:gentle)
     @visitor2.add_preference(:gentle)
     @ride1.board_rider(@visitor1)
     @ride1.board_rider(@visitor2)
     @ride1.board_rider(@visitor1)
-
+    
     expect(@ride1.rider_log).to eq({@visitor1 => 2, @visitor2 => 1})
+  end
+  
+  it 'can total the revenue of the ride' do
+    @ride1.board_rider(@visitor1)
+    @ride1.board_rider(@visitor2)
+    
+    expect(@visitor1.spending_money).to eq(8)
+    expect(@visitor2.spending_money).to eq(4)
+    expect(@ride1.total_revenue).to eq(3)
   end
 
 end
