1da9fb1ac7aed5c306c5634ca77122d7f023a434
Nathan Lambertson <lambertones@gmail.com>
2023-11-14 12:10:25 -0600

    test: Add test for #summary

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -115,7 +115,7 @@ RSpec.describe Carnival do
     end
   end
 
-  describe 'total_revenue_from_all_rides' do
+  describe '#total_revenue_from_all_rides' do
     it 'returns an Integer of the carnivals global ride revenue' do
       carnival = Carnival.new(14)
       ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
@@ -149,8 +149,47 @@ RSpec.describe Carnival do
       ride2.board_ride(visitor3)
       ride2.board_ride(visitor3)
       ride2.board_ride(visitor3)
-      
+
       expect(carnival.total_revenue_from_all_rides).to eq(28)
+    end
+  end
+
+  describe '#summary' do
+    it 'returns a hash of :visitor_count, :revenue_earned, :most_popular_ride, :most_profitable_ride, and :total_revenue' do
+      carnival = Carnival.new(14)
+      ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+      visitor1 = Visitor.new('Bruce', 54, '$12')
+      visitor2 = Visitor.new('Tucker', 36, '$5')
+
+      visitor1.add_preference(:gentle)
+      visitor2.add_preference(:gentle)
+
+      ride1.board_ride(visitor1)
+      ride1.board_ride(visitor2)
+      ride1.board_ride(visitor1)
+
+      expect(ride1.rider_log).to eq({visitor1 => 2, visitor2 => 1})
+      expect(visitor1.spending_money).to eq(10)
+      expect(visitor2.spending_money).to eq(4)
+      expect(ride1.total_revenue).to eq(3)
+
+      visitor3 = Visitor.new('Penny', 64, '$15')
+      ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+      ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+
+      visitor3.add_preference(:gentle)
+
+      carnival.add_ride(ride1)
+      carnival.add_ride(ride2)
+      carnival.add_ride(ride3)
+
+      ride2.board_ride(visitor1)
+      ride2.board_ride(visitor1)
+      ride2.board_ride(visitor3)
+      ride2.board_ride(visitor3)
+      ride2.board_ride(visitor3)
+
+      expect(carnival.summary).to be_a Hash
     end
   end
 end
