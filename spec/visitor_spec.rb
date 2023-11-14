0c3e208a237a532fa7659ed00d29cccec357705e
Stephen Nash <stephen.aa.nash@gmail.com>
2023-11-14 12:24:01 -0500

    refactor: Update to correctly output spending money to an integer

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Visitor do
   it 'has attributes' do
     expect(@visitor1.name).to eq('Bruce')
     expect(@visitor1.height).to eq(54)
-    expect(@visitor1.spending_money).to eq('$10')
+    expect(@visitor1.spending_money).to eq(10)
     expect(@visitor1.preferences).to eq([])
   end
 
