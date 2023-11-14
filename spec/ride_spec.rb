bc05a4c76ae3671196c38cc4a228d8b07125dc38
Martin Chavez <martinchavez1203@gmail.com>
2023-11-14 11:05:48 -0700

    #board_rider method & #total_revenue method functions with test passing

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -6,8 +6,9 @@ RSpec.describe Ride do
     @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
     @visitor1 = Visitor.new('Bruce', 54, '$10')
     @visitor2 = Visitor.new('Tucker', 36, '$5')
-
-
+    @visitor3 = Visitor.new('Penny', 64, '$15')
+    @ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+    @ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
   end
 
   describe '#initialize' do
@@ -27,7 +28,7 @@ RSpec.describe Ride do
     end
   end
 
-  describe '#board_rider' do
+  describe '#board_rider & total_revenue' do
     it 'boards a rider' do
       @visitor1.add_preference(:gentle)
       @visitor2.add_preference(:gentle)
@@ -36,11 +37,35 @@ RSpec.describe Ride do
       @ride1.board_rider(@visitor2)
       @ride1.board_rider(@visitor1)
 
-      expected_rider_log = {
+      expected = {
         @visitor1 => 2,
         @visitor2 => 1
       }
-      expect(@ride1.rider_log).to eq(expected_rider_log)
+      expect(@ride1.rider_log).to eq(expected)
+
+      expect(@visitor1.spending_money).to eq(8)
+      expect(@visitor2.spending_money).to eq(4)
+      expect(@ride1.total_revenue).to eq(3)
+
+      @visitor2.add_preference(:thrilling)
+      @visitor3.add_preference(:thrilling)
+
+      @ride3.board_rider(@visitor1)
+
+      @ride3.board_rider(@visitor2)
+
+      @ride3.board_rider(@visitor3)
+
+      expect(@visitor1.spending_money).to eq(8)
+      expect(@visitor2.spending_money).to eq(4)
+      expect(@visitor3.spending_money).to eq(13)
+
+      expected_log = {
+        @visitor3 =>1
+      }
+
+      expect(@ride3.rider_log).to eq(expected_log)
+      expect(@ride3.total_revenue).to eq(2)
     end
   end
 end
