69ececc133ca33f2cc6fe8648245114906089825
Dana Howell <howelld115@gmail.com>
2023-11-14 11:24:08 -0700

    Add #board_rider

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,11 +1,29 @@
 class Ride
-  attr_reader :name, :min_height, :admission_fee, :excitement
+  attr_reader :name, 
+              :min_height,
+              :admission_fee,
+              :excitement,
+              :total_revenue,
+              :rider_log
   
   def initialize(attributes)
     @name = attributes[:name]
     @min_height = attributes[:min_height]
     @admission_fee = attributes[:admission_fee]
     @excitement = attributes[:excitement]
+    @total_revenue = 0
+    @rider_log = {}
+  end
 
+  def board_rider(visitor)
+    if !@rider_log.include?(visitor)
+      @rider_log[visitor] = 1
+    else
+      @rider_log[visitor] += 1
+    end
+    #Add to rider_log[visitor] += num_rides accumulated
+
+    #add rider if rider isn't in hash.  If rider is in hash, += rides_accumulated
+  
   end
 end
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -7,6 +7,32 @@ RSpec.describe Ride do
     it "has @name, @min_height, @admission_fee, @excitement" do
       ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
 
+    expect(ride1).to be_an_instance_of(Ride)
+    expect(ride1.name).to eq("Carousel")
+    expect(ride1.min_height).to eq(24)
+    expect(ride1.admission_fee).to eq(1)
+    expect(ride1.excitement).to eq(:gentle)
+    expect(ride1.total_revenue).to eq(0)
+    end
+
+    it "has visitors with preferences" do
+      ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+      visitor1 = Visitor.new('Bruce', 54, '$10')
+      visitor2 = Visitor.new('Tucker', 36, '$5')
+      
+      visitor1.add_preference(:gentle)
+      visitor2.add_preference(:gentle)
+
+      expect(visitor1.preferences).to eq([:gentle])
+      expect(visitor2.preferences).to eq([:gentle])
+      
+      ride1.board_rider(visitor1)
+      ride1.board_rider(visitor2)
+
+      expect(ride1.rider_log).to eq({visitor1 => 1, visitor2 => 1})
+      #expect(ride1.rider_log).to eq({visitor1, visitor2})
+      
+
     end
   end
 end
