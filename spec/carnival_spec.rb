a3f18919d6dd3ca16e093d417b1f7cc9f68f970f
Nathan Lambertson <lambertones@gmail.com>
2023-11-14 11:52:03 -0600

    test: Add test for #most_profitable_ride

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -76,5 +76,42 @@ RSpec.describe Carnival do
     end
   end
 
-  describe
+  describe '#most_profitable_ride' do
+    it 'returns the ride with highest earning' do
+      carnival = Carnival.new(14)
+      ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+      visitor1 = Visitor.new('Bruce', 54, '$12')
+      visitor2 = Visitor.new('Tucker', 36, '$5')
+
+      visitor1.add_preference(:gentle)
+      visitor2.add_preference(:gentle)
+
+      ride1.board_ride(visitor1)
+      ride1.board_ride(visitor2)
+      ride1.board_ride(visitor1)
+
+      expect(ride1.rider_log).to eq({visitor1 => 2, visitor2 => 1})
+      expect(visitor1.spending_money).to eq(10)
+      expect(visitor2.spending_money).to eq(4)
+      expect(ride1.total_revenue).to eq(3)
+
+      visitor3 = Visitor.new('Penny', 64, '$15')
+      ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+      ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+
+      carnival.add_ride(ride1)
+      carnival.add_ride(ride2)
+      carnival.add_ride(ride3)
+
+      ride2.board_ride(visitor1)
+      ride2.board_ride(visitor1)
+      ride2.board_ride(visitor3)
+      ride2.board_ride(visitor3)
+      ride2.board_ride(visitor3)
+
+      visitor3.add_preference(:gentle)
+
+      expect(most_profitable_ride).to eq(ride2)
+    end
+  end
 end
