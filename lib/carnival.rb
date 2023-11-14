d8240cda252f8516dd3f413a80fc1ed8fa35465e
Dylan Perry <dylanbperry@gmail.com>
2023-11-14 09:51:45 -0800

    Add method and test for most_popular_ride

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -9,4 +9,16 @@ class Carnival
     def add_ride(ride)
         @rides << ride
     end
+
+    def most_popular_ride
+        ride_with_times_ridden = {}
+        @rides.map do |ride|
+            total_times_ridden = 0
+            ride.rider_log.map do |rider, times_ridden|
+                total_times_ridden += times_ridden
+            end
+            ride_with_times_ridden[ride] = total_times_ridden
+        end
+        ride_with_times_ridden.sort_by {|ride, times_ridden| times_ridden}.last[0]
+    end
 end
