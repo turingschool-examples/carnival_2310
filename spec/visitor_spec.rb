1fd826142cb40fc5af2dc1f7fef763a01fdd50f2
Martin Chavez <martinchavez1203@gmail.com>
2023-11-14 09:30:55 -0700

    set test up for #tall_enough method

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -4,6 +4,8 @@ require './lib/visitor'
 RSpec.describe Visitor do
   before(:each) do
     @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @visitor3 = Visitor.new('Penny', 64, '$15')
   end
 
   describe '#initialize' do
@@ -27,6 +29,15 @@ RSpec.describe Visitor do
       @visitor1.add_preference(:gentle)
       @visitor1.add_preference(:thrilling)
       expect(@visitor1.preferences).to eq([:gentle, :thrilling])
+    end
+  end
+
+  describe '#tall_enough?' do
+    it 'checks if they are tall enough for rides' do
+      expect(@visitor1.tall_enough?(54)).to be(true)
+      expect(@visitor2.tall_enough?(54)).to eq(false)
+      expect(@visitor3.tall_enough?(54)).to eq(true)
+      expect(@visitor1.tall_enough?(64)).to eq(false)
     end
   end
 end
