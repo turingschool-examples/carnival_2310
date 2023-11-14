980561330977c2d9700ba70a46d7d3836a94f290
Stephen Nash <stephen.aa.nash@gmail.com>
2023-11-14 11:48:40 -0500

    test: Create test for ride attributes

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -9,7 +9,14 @@ RSpec.describe Ride do
   it 'exists' do
     expect(@ride1).to be_an_instance_of(Ride)
   end
-
+  
+  it 'has attributes' do
+    expect(@ride1.name).to eq('Carousel')
+    expect(@ride1.min_height).to eq(24)
+    expect(@ride1.admission_fee).to eq(1)
+    expect(@ride1.excitement).to eq(:gentle)
+    expect(@ride1.total_revenue).to eq(0)
+  end
 
 
 end
