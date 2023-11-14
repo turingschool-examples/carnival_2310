da55544f4cbfde2b51eae3e393adf3cc11c67c76
Stephen Nash <stephen.aa.nash@gmail.com>
2023-11-14 11:27:27 -0500

    test: Create tests for attributes

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -11,12 +11,12 @@ RSpec.describe Visitor do
     expect(@visitor1).to be_an_instance_of(Visitor)
   end
 
-  # it 'has attributes' do
-  #   expect(@visitor1.name).to eq('Bruce')
-  #   expect(@visitor1.height).to eq(54)
-  #   expect(@visitor1.spending_money).to eq('$10')
-  #   expect(@visitor1.preferences).to eq([])
-  # end
+  it 'has attributes' do
+    expect(@visitor1.name).to eq('Bruce')
+    expect(@visitor1.height).to eq(54)
+    expect(@visitor1.spending_money).to eq('$10')
+    expect(@visitor1.preferences).to eq([])
+  end
 
   # it 'can add to the array of preferences' do
   #   @visitor1.add_preference(:gentle)
