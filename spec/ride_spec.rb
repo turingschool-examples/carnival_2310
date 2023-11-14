969e5924cb6b5b44331a635437628d0a265eab7d
Stephen Nash <stephen.aa.nash@gmail.com>
2023-11-14 11:55:10 -0500

    test: Create test to board_riders into the rider_log

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -18,5 +18,17 @@ RSpec.describe Ride do
     expect(@ride1.total_revenue).to eq(0)
   end
 
+  it 'can board riders into the rider log' do
+    @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @visitor1.add_preference(:gentle)
+    @visitor2.add_preference(:gentle)
+    @ride1.board_rider(visitor1)
+    @ride1.board_rider(visitor2)
+    @ride1.board_rider(visitor1)
+
+    expect(@ride1.rider_log).to eq({'#<Visitor:0x000000015a16e918 @height=54, @name="Bruce", @preferences=[:gentle], @spending_money=8> => 2,
+    #<Visitor:0x000000015a11c5c8 @height=36, @name="Tucker", @preferences=[:gentle], @spending_money=4> => 1'})
+  end
 
 end
