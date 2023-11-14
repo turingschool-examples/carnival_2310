1bb4371453aa1e147721e18fe981509839f54780
Barry Allahyar <barrya@gmail.com>
2023-11-14 10:34:41 -0600

    test: create respective readable attributes test for #visitor

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
-describe Visitor do
-  it describe "initialize" do
-    visitor1 = Visitor.new('Bruce', 54, '$10')
-    expect(visitor1).to be_instance_of(Vistor)
-  end
-
-
+RSpec.describe Visitor do
   
+  describe "#initialize" do
+    it 'can create an instance of visitor' do
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+
+    expect(visitor1).to be_instance_of(Vistor)
+    end
+
+    it "has readiable attributes" do
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+      
+    expect(visitor1.name).to eq("Bruce")
+    expect(visitor1.height).to eq(54)
+    expect(visitor1.spending_money).to eq(10)
+    expect(visitor1.preferences).to eq([])
+    end
+  end
 end
