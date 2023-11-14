b5339bec1c0323e856b9e25340f0a2c11c99ddd8
Meg Roth <megroth2@gmail.com>
2023-11-14 09:15:41 -0700

    add tall_enough? test to visitor spec

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -21,8 +21,15 @@ RSpec.describe Visitor do
     expect(visitor1.preferences).to eq([:gentle, :thrilling])
   end
 
-  # it 'can determine if visitor is tall enough' do
+  it 'can determine if visitor is tall enough' do
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+    visitor2 = Visitor.new('Tucker', 36, '$5')
+    visitor3 = Visitor.new('Penny', 64, '$15')
 
-  # end
+    expect(visitor1.tall_enough?(54)).to eq(true)
+    expect(visitor2.tall_enough?(54)).to eq(false)
+    expect(visitor3.tall_enough?(54)).to eq(true)
+    expect(visitor1.tall_enough?(64)).to eq(false)
+  end
 
 end
