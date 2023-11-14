8e3e48cfb55504972323fa5c040c85fd273e3e92
Stephen Nash <stephen.aa.nash@gmail.com>
2023-11-14 13:23:32 -0500

    test: Create test for attributes

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -5,10 +5,19 @@ RSpec.describe Carnival do
 
   before(:each) do
     @carnival = Carnival.new('Winter Wonderland', 7)
+    @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+    @ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+    @ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
   end
 
   it 'exists' do
     expect(@carnival).to be_an_instance_of(Carnival)
+  end
+
+  it 'has attributes' do
+    expect(@carnival.name).to eq('Winter Wonderland')
+    expect(@carnival.duration).to eq(7)
+    expect(@carnival.rides).to eq([])
   end
 
 
