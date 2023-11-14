b14a65139a28ba74602a3f04f70995174d2483d3
Meg Roth <megroth2@gmail.com>
2023-11-14 09:27:46 -0700

    add initialize method for ride class

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,8 +1,16 @@
 class Ride
-  attr_reader 
+  attr_reader :name,
+              :min_height,
+              :admission_fee,
+              :excitement,
+              :rider_log
 
-  def initialize
-
+  def initialize(ride_details)
+    @name = ride_details[:name]
+    @min_height = ride_details[:min_height]
+    @admission_fee = ride_details[:admission_fee]
+    @excitement = ride_details[:excitement]
+    @rider_log = {}
   end
 
 end
