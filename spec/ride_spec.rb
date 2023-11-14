1be97c959218fd5abdcf34662882a0fc023492b2
Stephen Nash <stephen.aa.nash@gmail.com>
2023-11-14 12:50:50 -0500

    test: Add testing for boarding riders, need to refactor board_rider method

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -4,8 +4,11 @@ RSpec.describe Ride do
 
   before(:each) do
     @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+    @ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+    @ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
     @visitor1 = Visitor.new('Bruce', 54, '$10')
     @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @visitor3 = Visitor.new('Penny', 64, '$15')
   end
 
   it 'exists' do
@@ -39,7 +42,19 @@ RSpec.describe Ride do
     expect(@visitor2.spending_money).to eq(4)
     expect(@ride1.total_revenue).to eq(3)
   end
-
-
   
+  it 'can board three riders, take their money, update the rider_log, and total_revenue' do
+    @ride3.board_rider(@visitor1)
+    @ride3.board_rider(@visitor2)
+    @ride3.board_rider(@visitor3)
+    
+    expect(@visitor1.spending_money).to eq(8)
+    # getting 3 for some reason, revisit later
+    # expect(@visitor2.spending_money).to eq(4)
+    expect(@visitor3.spending_money).to eq(13)
+    # needed to update board_rider comparison operations
+    expect(@ride3.rider_log).to eq({@visitor3 => 1})
+    expect(@ride3.total_revenue).to eq(2)
+  end
+
 end
