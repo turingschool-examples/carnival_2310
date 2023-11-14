693facd872791dee9cbfc3c05a5ccae869ed0b94
Martin Chavez <martinchavez1203@gmail.com>
2023-11-14 09:17:56 -0700

    add test for attributes

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -11,4 +11,13 @@ RSpec.describe Visitor do
       expect(@visitor1).to be_a(Visitor)
     end
   end
+
+  describe 'attributes' do
+    it 'reads attributes and returns them' do
+      expect(@visitor1.name).to eq("Bruce")
+      expect(@visitor1.height).to eq(54)
+      expect(@visitor1.height).to eq(10)
+      expect(@visitor1.preferences).to eq([])
+    end
+  end
 end
