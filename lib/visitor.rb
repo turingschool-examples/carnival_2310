39242ac19f60178c0a20823e78dfb03da0142124
Isaac Mitchell <tmitchellisaac@gmail.com>
2023-11-14 09:14:48 -0700

    Add visitor tests and visitor class with add preference and tall enough methods

---
+++ lib/visitor.rb
@@ -0,0 +1,25 @@
+
+
+class Visitor
+
+  attr_reader :name, :height, :spending_money, :preferences
+
+
+  def initialize(name, height, money)
+    @name = name
+    @height = height
+    @spending_money = money.delete("$").to_i
+    @preferences = []
+  end
+
+  def add_preference(preference)
+    @preferences << preference
+  end
+
+  def tall_enough?(given_height)
+    @height >= given_height
+  end
+
+
+
+end
---
+++ spec/visitor_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+require 'rspec'
+
+RSpec.describe Visitor do
+  it 'exists' do
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+    expect(visitor1).to be_a Visitor
+  end
+
+  it 'has a name and height and spending money and preferences' do
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+    expect(visitor1.name).to eq("Bruce")
+    expect(visitor1.height).to eq(54)
+    expect(visitor1.spending_money).to eq(10)
+    expect(visitor1.preferences).to eq([])
+  end
+
+  it "can add preferences" do
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+    expect(visitor1.preferences).to eq([])
+    visitor1.add_preference(:gentle)
+    visitor1.add_preference(:thrilling)
+    expect(visitor1.preferences).to eq([:gentle, :thrilling])
+
+  end
+
+it "has method to check if visitor is tall enough" do
+  visitor1 = Visitor.new('Bruce', 54, '$10')
+  visitor2 = Visitor.new('Tucker', 36, '$5')
+  visitor3 = Visitor.new('Penny', 64, '$15')
+  expect(visitor1.tall_enough?(54)).to eq(true)
+  expect(visitor2.tall_enough?(54)).to eq(false)
+  expect(visitor3.tall_enough?(54)).to eq(true)
+  expect(visitor1.tall_enough?(64)).to eq(false)
+end
+
+
+
+end
