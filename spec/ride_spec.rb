5475d665b945227bff248d044c669cfd9689b33f
Meg Roth <megroth2@gmail.com>
2023-11-14 09:37:41 -0700

    add board_rider test to ride spec

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -19,8 +19,24 @@ RSpec.describe Ride do
     expect(ride1.total_revenue).to eq(0)
   end
 
-  # it 'can board rider' do
+  it 'can board rider' do
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+    visitor2 = Visitor.new('Tucker', 36, '$5')
 
-  # end
+    visitor1.add_preference(:gentle)
+    visitor2.add_preference(:gentle)
+
+    ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+
+    ride1.board_rider(visitor1)
+    ride1.board_rider(visitor2)
+    ride1.board_rider(visitor1)
+
+    
+    expect(ride1.rider_log).to eq({visitor1=> 2, visitor2 => 1})
+    expect(visitor1.spending_money).to eq(8)
+    expect(visitor2.spending_money).to eq(4)
+    expect(ride1.total_revenue).to eq(3)
+  end
 
 end
