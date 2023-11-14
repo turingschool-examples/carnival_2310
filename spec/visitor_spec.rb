169fd52c17d2e6f18e69e3b5d59917064c643db9
Barry Allahyar <barrya@gmail.com>
2023-11-14 10:42:28 -0600

    test: Create tests for #add_preference

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Visitor do
     expect(visitor1).to be_instance_of(Vistor)
     end
 
-    it "has readiable attributes" do
+    it "has readable attributes" do
     visitor1 = Visitor.new('Bruce', 54, '$10')
       
     expect(visitor1.name).to eq("Bruce")
@@ -17,5 +17,24 @@ RSpec.describe Visitor do
     expect(visitor1.spending_money).to eq(10)
     expect(visitor1.preferences).to eq([])
     end
+
+    it 'can add preferences' do
+      visitor1 = Visitor.new('Bruce', 54, '$10')
+      visitor.add_preferences(:gentle)
+
+      expect(visitor.preferences).to eq([:gentle])
+    end
+
+    it 'can add an additional preference' do
+      visitor1 = Visitor.new('Bruce', 54, '$10')
+      visitor.add_preferences(:gentle)
+      visitor.add_preferences(:thrilling)
+
+      expect(visitor.preferences).to eq([:gentle])
+      expect(visitor.preferences).to eq([:gentle, :thrilling])
+    end
+
+
+    
   end
 end
