0875cdc2749dda26fdca0e00afcb7a563f98034b
Meg Roth <megroth2@gmail.com>
2023-11-14 10:18:37 -0700

    add total_revenue test to carnival spec

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -11,10 +11,10 @@ RSpec.describe Carnival do
   end
 
   it 'can add rides' do
-    carnival = Carnival.new(14)
-
     ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
     ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+    
+    carnival = Carnival.new(14)
 
     carnival.add_ride(ride1)
     
@@ -23,6 +23,34 @@ RSpec.describe Carnival do
     carnival.add_ride(ride2)
     
     expect(carnival.rides).to eq([ride1, ride2])
+  end
+
+  it 'can calculate the total revenue earned from all its rides' do
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+    visitor2 = Visitor.new('Tucker', 36, '$5')
+    visitor3 = Visitor.new('Penny', 64, '$15')
+
+    visitor1.add_preference(:gentle)
+    visitor2.add_preference(:gentle)
+    visitor3.add_preference(:gentle)
+    
+    ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+    ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+    
+    carnival = Carnival.new(14)
+
+    carnival.add_ride(ride1)
+    carnival.add_ride(ride2)
+
+    ride1.board_rider(visitor1) # total = 1
+    ride1.board_rider(visitor2) # total = 2
+    ride1.board_rider(visitor3) # total = 3
+
+    ride2.board_rider(visitor1) # total = 8
+    ride2.board_rider(visitor3) # total = 13
+    ride2.board_rider(visitor3) # total = 18
+
+    expect(carnival.total_revenue).to eq(18)
   end
 
 end
