87b28ac5286f47ead2f4297a67cbe61b395234fe
Meg Roth <megroth2@gmail.com>
2023-11-14 09:25:38 -0700

    add initialize test for ride spec

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -3,10 +3,14 @@ require 'spec_helper'
 RSpec.describe Ride do
 
   it 'exists' do
-    
+    ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
 
-    expect(ride).to be_a(Ride)
-    expect().to eq()
+    expect(ride1).to be_a(Ride)
+    expect(ride1.name).to eq("Carousel")
+    expect(ride1.min_height).to eq(24)
+    expect(ride1.admission_fee).to eq(1)
+    expect(ride1.excitement).to eq(:gentle)
+    expect(ride1.rider_log).to eq({})
   end
 
 end
