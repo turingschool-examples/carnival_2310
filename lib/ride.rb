5c5809e6be3543e5400e0c43694c81f00c85477e
Stephen Nash <stephen.aa.nash@gmail.com>
2023-11-14 11:49:13 -0500

    edit: Correct class atrributes to pass rspec test

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,12 +1,12 @@
 class Ride
-  attr_reader :name, :min_height, :admission_fee, :excitement
+  attr_reader :name, :min_height, :admission_fee, :excitement, :total_revenue
 
   def initialize(attributes)
     @name = attributes[:name]
     @min_height = attributes[:min_height]
     @admission_fee = attributes[:admission_fee]
     @excitement = attributes[:excitement]
-    @total_revenue = {}
+    @total_revenue = 0
   end
 
 
