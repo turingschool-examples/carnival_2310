34eb441d78e3b43c6e1aaaa4998605cc6c1a7a8d
Dylan Perry <dylanbperry@gmail.com>
2023-11-14 08:39:33 -0800

    Add total_revenue variable with default value of 0

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,10 +1,12 @@
 class Ride
-    attr_reader :name, :min_height, :admission_fee, :excitement
+    attr_reader :name, :min_height, :admission_fee, :excitement, :total_revenue
 
     def initialize(ride_hash)
         @name = ride_hash[:name]
         @min_height = ride_hash[:min_height]
         @admission_fee = ride_hash[:admission_fee]
         @excitement = ride_hash[:excitement]
+        @total_revenue = 0
     end
+
 end
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -12,10 +12,11 @@ describe Ride do
         expect(@ride1).to be_a Ride
     end
 
-    it "accepts hash arguments for name, min_height, admission_fee, and excitement" do
+    it "accepts hash arguments for name, min_height, admission_fee, and excitement, and initializes total_revenue variable with default 0" do
         expect(@ride1.name).to eq("Carousel")
         expect(@ride1.min_height).to eq(24)
         expect(@ride1.admission_fee).to eq(1)
         expect(@ride1.excitement).to eq(:gentle)
+        expect(@ride1.total_revenue).to eq(0)
     end
 end
