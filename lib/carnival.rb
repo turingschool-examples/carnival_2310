633720fef2447afd864aaa0ffb00eb1b464061d8
Dylan Perry <dylanbperry@gmail.com>
2023-11-14 09:20:07 -0800

    Add add_ride method and test

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -1,7 +1,12 @@
 class Carnival
-    attr_reader :duration
+    attr_reader :duration, :rides
 
     def initialize(duration)
         @duration = duration
+        @rides = []
+    end
+
+    def add_ride(ride)
+        @rides << ride
     end
 end
--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -4,6 +4,10 @@ require_relative "./spec_helper"
 describe Carnival do
     before(:all) do
         @carnival = Carnival.new(10)
+
+        @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+        @ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+        @ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
     end
 
     it "exists" do
@@ -12,5 +16,14 @@ describe Carnival do
 
     it "stores and reads a duration argument" do
         expect(@carnival.duration).to eq(10)
+    end
+
+    it "stores and reads a list of rides" do
+        expect(@carnival.rides).to eq([])
+
+        @carnival.add_ride(@ride1)
+        @carnival.add_ride(@ride2)
+
+        expect(@carnival.rides).to eq([@ride1, @ride2])
     end
 end
