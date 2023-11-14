137f6a1662b5a94a1f0718eabe3dbed6c1210317
Barry Allahyar <barrya@gmail.com>
2023-11-14 12:26:32 -0600

    test: add test to keep log of riders boarded as well as count of times rode per rider

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -9,14 +9,38 @@ RSpec.describe Ride do
       expect(ride1).to be_instance_of(Ride)
     end 
 
-    it "has name, min_height, admission_fee, excitement, as instance values within a hash" do 
+    it "can create a hash with respective ride attributes" do 
       ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
-      expect(ride1.name).to eq("Carousel")
+      expect(ride1.name).to eq('Carousel')
       expect(ride1.min_height).to eq(24)
       expect(ride1.admission_fee).to eq(1)
       expect(ride1.excitement).to eq (:gentle)
     end 
   end
 
+  it "can track total revenue" do 
+    ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+    
+    expect(ride1.total_revenue).to eq(0)
+  end
+
+  
+  it "can keep a log of riders boarded and count how many times they rode the ride" do
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+    visitor2 = Visitor.new('Tucker', 36, '$5')
+    visitor1.add_preference(:gentle)
+    visitor2.add_preference(:gentle)
+
+    expect(ride.rider_log).to eq({})
+
+    ride1.board_rider(visitor1)
+
+    expect(ride.rider_log).to eq({visitor1 => 1})
+    ride1.board_rider(visitor2)
+    ride1.board_rider(visitor1)
+    expect(ride.rider_log).to eq({visitor1 => 2, visitor2 => 1})
+
+
+  end
 
 end
