882939eec9a39786d32be308c7bacf9eb1a638a9
Stephen Nash <stephen.aa.nash@gmail.com>
2023-11-14 12:36:23 -0500

    refactor: Update total_revenue test correctly

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -33,10 +33,13 @@ RSpec.describe Ride do
   it 'can total the revenue of the ride' do
     @ride1.board_rider(@visitor1)
     @ride1.board_rider(@visitor2)
+    @ride1.board_rider(@visitor1)
     
     expect(@visitor1.spending_money).to eq(8)
     expect(@visitor2.spending_money).to eq(4)
     expect(@ride1.total_revenue).to eq(3)
   end
 
+
+  
 end
