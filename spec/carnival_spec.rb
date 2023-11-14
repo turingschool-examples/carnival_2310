d974f63042237fbc37a7d18e3aa2f6f29d18b3eb
Martin Chavez <martinchavez1203@gmail.com>
2023-11-14 11:21:28 -0700

    add test to instantiate Carnival class

---
+++ spec/carnival_spec.rb
@@ -0,0 +1,15 @@
+require './lib/visitor'
+require './lib/ride'
+require './lib/carnival'
+
+RSpec.describe Carnival do
+  before(:each) do
+    @carnival = Carnival.new()
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@carnival).to be_a(Carnival)
+    end
+  end
+end
