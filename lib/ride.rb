ddfc7021f9e608de3339cac7d101a275a5cb0ec0
Martin Chavez <martinchavez1203@gmail.com>
2023-11-14 09:55:02 -0700

    attributes can be read and returned

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,5 +1,13 @@
 class Ride
+  attr_reader :name, :min_height, :admission_fee, :excitement, :rider_log, :total_revenue
   def initialize(attributes)
-    
+    @name = attributes[:name]
+    @min_height = attributes[:min_height]
+    @admission_fee = attributes[:admission_fee]
+    @excitement = attributes[:excitement]
+    @rider_log = {}
+    @total_revenue = 0
+
+
   end
 end
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -11,4 +11,15 @@ RSpec.describe Ride do
       expect(@ride1).to be_a(Ride)
     end
   end
+
+  describe '#attributes' do
+    it 'reads and returns attributes' do
+      expect(@ride1.name).to eq("Carousel")
+      expect(@ride1.min_height).to eq(24)
+      expect(@ride1.admission_fee).to eq(1)
+      expect(@ride1.excitement).to eq(:gentle)
+      expect(@ride1.total_revenue).to eq(0)
+      expect(@ride1.rider_log).to eq({})
+    end
+  end
 end
